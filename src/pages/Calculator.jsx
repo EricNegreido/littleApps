48ab6c9de9ca389162c8331100a8676c6e0f1f48
Eric Negreido <ericnegreidooo@gmail.com>
2024-10-02 18:54:27 -0300

    <feature> simple style for the moment

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -2,26 +2,38 @@ import { Link } from "../Link"
 import '../pageModuls/calculator/style.css'
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-const operators = ["%", "X", "+", "-"]
+const operators = ["/", "x", "+", "-"]
+const footOperators = [0, ".", "AC", "+/-"]
 
 export function Calculator() {
   return (
     <>
       <h1> Calculator</h1>
-      <main>
+      <header>
         <input className="screen" type="number" name="" id="" />
-        <article className="board">
+      </header>
+      <main className="board">
+        <article className="numKeypad">
           {numbers.map((val, i) => {
             return <div className="numbers" key={i}> {numbers[i]} </div>
           })}
         </article>
-        <aside>
+        <aside className="opKeypad">
           {operators.map((val, i) => {
             return <div className="operators" key={i}> {operators[i]} </div>
           })}
         </aside>
-        <footer> 0 . =</footer>
       </main>
+      <footer className="footerOperators">
+        {footOperators.map((val, i) => {
+            if(i == 0){
+              return <div className="numbers" key={i}> {footOperators[i]} </div>
+
+            }
+            return <div className="footOperators" key={i}> {footOperators[i]} </div>
+          })}
+      </footer>
+
       <Link to='/'> Inicio </Link>
     </>)
 }
