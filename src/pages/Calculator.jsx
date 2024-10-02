8698e63deebfd4c3c9f856a2e90ce43888cc67b1
eric <ericnegreidooo@gmail.com>
2024-10-02 08:42:21 -0300

    <feature> style to calculator

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,13 +1,27 @@
 import { Link } from "../Link"
+import '../pageModuls/calculator/style.css'
 
-export function Calculator(){
-  return(
-  <> 
-    <h1> Calculator</h1> 
-    <header>  Pantalla</header>
-    <main>
-      numero 0-9 <article> operators</article>
-    </main>
-    <Link to='/'> Inicio </Link>
-  </>)
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const operators = ["%", "X", "+", "-"]
+
+export function Calculator() {
+  return (
+    <>
+      <h1> Calculator</h1>
+      <main>
+        <input className="screen" type="number" name="" id="" />
+        <article className="board">
+          {numbers.map((val, i) => {
+            return <div className="numbers" key={i}> {numbers[i]} </div>
+          })}
+        </article>
+        <aside>
+          {operators.map((val, i) => {
+            return <div className="operators" key={i}> {operators[i]} </div>
+          })}
+        </aside>
+        <footer> 0 . =</footer>
+      </main>
+      <Link to='/'> Inicio </Link>
+    </>)
 }
