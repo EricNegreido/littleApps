7dc522a2d196fce2de821e24c8899ea415df921e
eric <ericnegreidooo@gmail.com>
2024-10-09 08:10:28 -0300

    <feature> switch to lights

--- src/pageModuls/christmas Lights/christmasLights.jsx
+++ src/pageModuls/christmas Lights/christmasLights.jsx
@@ -1,16 +1,23 @@
+import {useState} from 'react'
 import { Link } from "../../Link";
 import './style.css';
 export function ChristmasLights(){
+  const [toggle, setToggle] = useState(false);
+  function handleCLick(){
+    setToggle(!toggle);
+    }
     return <>
       <h1> Christmas Lights </h1>
       <main className="stringLights">
-        <div className="light"></div>
-        <div className="light"></div>
-        <div className="light"></div>
-        <div className="light"></div>
-        <div className="light"></div>
-        <div className="light"></div>
-        <div className="light"></div> </main>
+        <div className={toggle ? 'light animationLights' : 'light'}></div>
+        <div className={toggle ? 'light secondAnimationLights' : 'light'}></div>
+        <div className={toggle ? 'light animationLights' : 'light'}></div>
+        <div className={toggle ? 'light secondAnimationLights' : 'light'}></div>
+        <div className={toggle ? 'light animationLights' : 'light'}></div>
+        <div className={toggle ? 'light secondAnimationLights' : 'light'}></div>
+        <div className={toggle ? 'light animationLights' : 'light'}></div> 
+      </main>
+      <button className='switch' onClick={handleCLick}> On/off </button>
       <Link to='/'> Inicio </Link>
     </>
 }
